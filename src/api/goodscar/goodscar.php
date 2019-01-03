472b324341192f6bcfa31816a4b6b0bd4228e34d
Your Name <you@example.com>
2019-01-03 09:17:29 +0800

    购物车加减，初初稿”
    git commit -m 购物车加减，初初稿”

--- src/api/goodscar/goodscar.php
+++ src/api/goodscar/goodscar.php
@@ -5,10 +5,13 @@ $logPing = isset($_GET["logPing"])?$_GET["logPing"]:null;
 $logJie = isset($_GET["logJie"])?$_GET["logJie"]:null;
 $costZhe = isset($_GET["costZhe"])?$_GET["costZhe"]:null;
 $num = isset($_GET["num"])?$_GET["num"]:null;
-// echo $num;
+
 $toToprice = isset($_GET["toToprice"])?$_GET["toToprice"]:null;
 $accnum = isset($_GET["accnum"])?$_GET["accnum"]:null;
 $show = isset($_GET["show"])?$_GET["show"]:null;
+$amountVal = isset($_GET["amountVal"])?$_GET["amountVal"]:null;
+$totalVal = isset($_GET["totalVal"])?$_GET["totalVal"]:null;
+$accout = isset($_GET["accout"])?$_GET["accout"]:null;
 
 
 $cfirm = isset($_GET["cfirm"])?$_GET["cfirm"]:null;
@@ -32,15 +35,13 @@ $cfirm = isset($_GET["cfirm"])?$_GET["cfirm"]:null;
         $res = $conn -> query('select * from goodscar');
         $con = $res -> fetch_all(MYSQL_ASSOC);
         echo json_encode($con,JSON_UNESCAPED_UNICODE);
+    }else if($show == 2){
+        $res = $conn -> query('update goodscar set num="'.$amountVal.'",total="'.$totalVal.'"  where accout="'.$accout.'"');
+       
     }
     
     
-    //     $con = $res -> fetch_all(MYSQL_ASSOC);
-    // if($cfirm){
-    //     $res = $conn -> query('insert into goodscar (id,num,accout,total,smallImg,logPing,logJie,costZhe) values ("'.$guid.'","'.$num.'","'.$accnum.'","'.$toToprice.'","'.$smallImg.'","'.$logPing.'","'.$logJie.'","'.$costZhe.'")');
-    //         echo 666;
-    // }
-    
+   
 
 
     
--- src/js/goodscar/goodscar.js
+++ src/js/goodscar/goodscar.js
@@ -1,29 +1,20 @@
-jQuery(function(){
+jQuery(function () {
   var $car = $(".pick_car").find("ul").next("div");
   var show = 1;
-  $.getJSON("../../api/goodscar/goodscar.php",{"show":show},function(res){
-   
-    arr(res);
- 
+  $.getJSON("../../api/goodscar/goodscar.php", { "show": show }, (res) => {
+
+    vray(res);
+    
+    plusminus();//加减
+    spaceoOut();//增加
+
   })
 
-   // function arr(lise){
-   //  var $cm = "";
-   //  for (i = 0; i < lise.length; i++) {
-   //        var item = lise[i];
-   //  $(".minute_list_as").find("ul").next("img").arrt("src",${item.smallImg});      
-   //  $("letter").html(${item.logPing}&nbsp;${item.logJie})
-   //  $("unitPrice").html(${item.costZhe})
-   //  $("gross").html("￥"+ ${item.total})       
-   //      }
-
-
-
-   function arr(lise){
+  function vray(lise) {
     var $cm = "";
-        for (i = 0; i < lise.length; i++) {
-          var item = lise[i];
-          $cm += ` <ul class="minute">
+    for (i = 0; i < lise.length; i++) {
+      var item = lise[i];
+      $cm += ` <ul class="minute">
                   <li class="minute_list minute_list_as">
                     <input type="checkbox" />
                     <img src="${item.smallImg}" alt="" />
@@ -38,27 +29,79 @@ jQuery(function(){
                   </li>
                   <li class="minute_list minute_list_cs">
                     <span>￥</span>
-                    <span>${item.costZhe}</span>
+                    <i class="at">${item.costZhe}</i>
                   </li>
                   <li class="minute_list .minute_list_ds">
                     <div class="p-quantity">
                       <a href="javascript:;" class="btn-decrease">-</a>
-                      <input type="text" name="" value="${item.num}">
+                      <input type="text" name="" value="${item.num}" class="amount">
                       <a href="javascript:;" class="btn-increase">+</a>
                     </div>
                   </li>
                   <li class="minute_list minute_list_es">
-                    <span>￥${item.total}</span>
+                    <span>￥<i class="total" style="display: inline;">${item.total}</i></span>
                     <span>商品已包税</span>
                   </li>
-                  <li class="minute_list minute_list_fs"><a href="">删除</a>
+                  <li class="minute_list minute_list_fs"><a href="javascript:;">删除</a>
                   </li>
                   </ul>`;
-                };
-                $car.html($cm);
-        var $btn_decrease = $(".btn-decrease")
+    }
+    $car.html($cm);
+  }
 
-        }
+  //点击减,减去数量，且减数据库里面数量
+  function plusminus(){
+    var $decreaseBtn = $(".btn-decrease");
+    $decreaseBtn.on("click",function(){
+      var $total = $(this).parent().parent().next().children("span").children("i");
+   
+      var $at = $(this).parent().parent().prev().children("i").html();
+      var $amount = $(this).next();
+      var $amountVal = $(this).next().val();
+      if($amountVal > 0){
+       $amountVal --;
+      }
+      $amount.val($amountVal);
+      //点击减，小计金额改变
+      $total.html($amountVal * $at);
+      //数据库内容加减
+      var $show = 2;
+      var $totalVal = $total.html();
+      var $accout = Cookie.getCookie("enterVal");
+      console.log($accout)
+      
+            console.log($totalVal)
+      $.get("../../api/goodscar/goodscar.php",{"amountVal":$amountVal,"totalVal":$totalVal,"show":$show,"accout":$accout},function(res){
+
+      })
+    })
+  }
+
+
+  //点击减,增加数量，且加数据库里面数量  
+  function spaceoOut(){
+    var $increaseBtn = $(".btn-increase")
+    $increaseBtn.on("click",function(){
+      var $total = $(this).parent().parent().next().children("span").children("i");
+      var $at = $(this).parent().parent().prev().children("i").html();
+      var $amount = $(this).prev();
+      var $amountVal = $(this).prev().val();
+      $amountVal ++;
+      $amount.val($amountVal)
+      //点击加，小计金额改变
+      $total.html($amountVal * $at);
+      //数据库内容加减
+      var $show = 2;
+      var $accout = Cookie.getCookie("enterVal");
+      console.log($accout)
+     
+      var $totalVal = $total.html();
+      console.log($totalVal)
+      $.get("../../api/goodscar/goodscar.php",{"amountVal":$amountVal,"totalVal":$totalVal,"show":$show,"accout":$accout},function(res){
+      
+      })
+    })
+  }
 
 
   // ======================================
